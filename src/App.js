290e479029f21da3fe361e14750fdc02f413405d
Maykel Contreras Camacho <jotasenator@gmail.com>
2020-12-02 18:02:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         < div className="App">
           <Navbar />
           <Switch>
-            <Route path='jotasenator.github.io/blogjotas/' component={Inicio} />
+            <Route exact path='/' component={Inicio} />
             <Route path='/python' component={MyHistoryPython} />
             <Route path='/javascript' component={MyHistoryJS} />
             <Route path='/contacto' component={Contacto} />
