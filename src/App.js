f4bf651c615b352c337ccf3d98478544352968cb
Maykel Contreras Camacho <jotasenator@gmail.com>
2020-12-02 19:25:49 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         < div className="App">
           <Navbar />
           <Switch>
-            <Route exact path='/' component={Inicio} />
+            <Route exact path='/blogjotas/' component={Inicio} />
             <Route path='/python' component={MyHistoryPython} />
             <Route path='/javascript' component={MyHistoryJS} />
             <Route path='/contacto' component={Contacto} />
