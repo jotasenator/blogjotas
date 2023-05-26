f56eb9c244ae614c822bc2882f89e7fcb1bc51db
Maykel <jotasenator@gmail.com>
2023-05-26 18:45:58 -0400

    simple test

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -2,41 +2,40 @@ import React from 'react';
 import Slider from './carousel';
 import { useTranslation } from 'react-i18next';
 
-
-
-const Inicio = () => {
-    const { t, i18n } = useTranslation('global');
+const Inicio = () =>
+{
+    const { t, i18n } = useTranslation( 'global' );
     return (
         <div className='container'>
 
-            <h1>{t('inicio.p1')}</h1>
+            <h1>{ t( 'inicio.p1' ) }</h1>
 
             <div className='pInicio'>
 
                 <Slider />
 
                 <p>
-                    {t('inicio.p2')}
+                    { t( 'inicio.p2' ) }
                 </p>
 
                 <p>
-                    {t('inicio.p3')}
+                    { t( 'inicio.p3' ) }
                 </p>
 
                 <p>
-                    {t('inicio.p4')}
+                    { t( 'inicio.p4' ) }
                 </p>
 
                 <p>
-                    {t('inicio.p5')}
+                    { t( 'inicio.p5' ) }
                 </p>
 
                 <p>
-                    {t('inicio.p6')}
+                    { t( 'inicio.p6' ) }
                 </p>
 
                 <p className='pRef-inicio'>
-                    {t('inicio.p7')} <a href='https://www.proveyourworth.net/level3/start'
+                    { t( 'inicio.p7' ) } <a href='https://www.proveyourworth.net/level3/start'
                         target='_blank' rel=' nofollow noopener noreferrer'>
                         https://www.proveyourworth.net/level3/start</a>.
                 </p>
@@ -47,7 +46,7 @@ const Inicio = () => {
 
         </div >
 
-    )
-}
+    );
+};
 
-export default Inicio
+export default Inicio;
